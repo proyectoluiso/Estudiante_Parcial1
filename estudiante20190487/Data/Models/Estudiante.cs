fc6421a4b43ad9244304270334165309e3d9e5b9
proyectoluiso <117935580+proyectoluiso@users.noreply.github.com>
2022-11-11 22:07:44 -0400

    Add files via upload

---
+++ estudiante20190487/Data/Models/Estudiante.cs
@@ -0,0 +1,15 @@
+namespace estudiante20190487.Data.Models;
+public class estudiante
+
+{
+   public string? estudianteID {get;set;} 
+   public string? nombre {get;set;} 
+
+public string? apellido {get;set;} 
+
+public string? fechanacimiento {get;set;} 
+
+public string? matricula {get;set;} 
+public string? carrera {get;set;} 
+
+}
